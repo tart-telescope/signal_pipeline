54644561a3e4b6296718abec14d0eb9498ba062d
tim <tim@elec.ac.nz>
2024-01-04 15:21:55 +1300

    Fix script

--- lib/verilog-axis.bash
+++ lib/verilog-axis.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Use TMP dir to clone
 TMP=`mktemp --directory`
-cd ${TMP}
+pushd ${TMP}
 git clone https://github.com/alexforencich/verilog-axis.git
+popd
 cp -a ${TMP}/verilog-axis/rtl verilog-axis/
+echo "removing temp directory ${TMP}"
 rm -rf ${TMP}
