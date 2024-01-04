19d9537a6bd423a9f7635e839afb5440e246bf4f
tim <tim@elec.ac.nz>
2024-01-04 17:18:20 +1300

    Use specific commit hashes

--- lib/verilog-axis.bash
+++ lib/verilog-axis.bash
@@ -3,6 +3,8 @@
 TMP=`mktemp --directory`
 pushd ${TMP}
 git clone https://github.com/alexforencich/verilog-axis.git
+cd verilog-axis
+git checkout 7823b916bfd298441d5fdabf0b03d0ae8dc210e8
 popd
 cp -a ${TMP}/verilog-axis/rtl verilog-axis/
 echo "removing temp directory ${TMP}"
