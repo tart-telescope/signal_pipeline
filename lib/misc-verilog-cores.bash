cb975f3fae80caad34e9edebbb08acda3428a00d
Patrick Suggate <patrick.suggate@gmail.com>
2024-09-27 22:51:09 +1200

    WIP. Plumbing in the raw-data acquisition unit and the DDR3 SDRAM controller.

--- lib/misc-verilog-cores.bash
+++ lib/misc-verilog-cores.bash
@@ -4,10 +4,15 @@ TMP=`mktemp --directory`
 pushd ${TMP}
 git clone --depth=1 https://github.com/psuggate/misc-verilog-cores.git
 cd misc-verilog-cores
-git checkout 4c60fee3a40dbcedb835e06663c41ea02c4abfc2
+git checkout ed1c8fe27075eac9a9282aa884395d53621a42e8
 popd
 mkdir -p misc-verilog-cores
-cp -a ${TMP}/misc-verilog-cores/rtl/* misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/arch misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/axis misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/fifo misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/misc misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/spi  misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/uart misc-verilog-cores/
 cp -a ${TMP}/misc-verilog-cores/*.md misc-verilog-cores/
 cp -a ${TMP}/misc-verilog-cores/driver misc-verilog-cores/
 echo "removing temp directory ${TMP}"
